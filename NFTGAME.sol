240ce74284ed0d3f0369f5ec9083e6ae25ec1ad2
Ben Bk <35254747+BenBktech@users.noreply.github.com>
2021-12-04 18:59:56 +0100

    Update NFTGAME.sol

--- NFTGAME.sol
+++ NFTGAME.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Game is ERC721, Ownable {
+contract NFTGAME is ERC721, Ownable {
 
     enum type_character { VIKING, GAULOIS }
 
@@ -26,7 +26,7 @@ contract Game is ERC721, Ownable {
 
     }
 
-    function getTokenDetails(uint _tokenId) public view returns (Character memory) {
+    function getTokenDetails(uint _tokenId) public view returns(Character memory) {
         return _characterDetails[_tokenId];
     }
 
@@ -34,13 +34,13 @@ contract Game is ERC721, Ownable {
         require(balanceOf(msg.sender) <= 4, "Already create max characters.");
         require(_typeCharacter == type_character.VIKING || _typeCharacter == type_character.GAULOIS, "Not valid");
         if(_typeCharacter == type_character.VIKING) {
-            Character memory thisCharacter = Character(20, 15, 100, 1, block.timestamp, 0, type_character.VIKING);
+            Character memory thisCharacter = Character(20, 15, 100, 1, block.timestamp, block.timestamp, type_character.VIKING);
             _characterDetails[nextId] = thisCharacter;
             _safeMint(msg.sender, nextId);
             nextId++;
         }
         if(_typeCharacter == type_character.GAULOIS) {
-            Character memory thisCharacter = Character(13, 25, 80, 1, block.timestamp, 0, type_character.GAULOIS);
+            Character memory thisCharacter = Character(13, 25, 80, 1, block.timestamp, block.timestamp, type_character.GAULOIS);
             _characterDetails[nextId] = thisCharacter;
             _safeMint(msg.sender, nextId);
             nextId++;
@@ -51,53 +51,51 @@ contract Game is ERC721, Ownable {
         require(ownerOf(_tokenId) == msg.sender, "You cannot heal an other Character than yours.");
         Character storage thisCharacter = _characterDetails[_tokenId];
         require(thisCharacter.lastHeal + 60 < block.timestamp, "To soon to heal.");
-        require(thisCharacter.life > 0, "Can't heal someone that is already dead.");
+        require(thisCharacter.life > 0, "Cant't heal someone that is dead.");
         thisCharacter.lastHeal = block.timestamp;
         thisCharacter.life += 50;
     }
 
     function fight(uint _tokenId1, uint _tokenId2) public payable {
         require(_characterDetails[_tokenId1].lastFight + 60 < block.timestamp && _characterDetails[_tokenId2].lastFight + 60 < block.timestamp, "Must wait before next fight");
-        require(ownerOf(_tokenId1) == msg.sender, "Not your character.");
-        require(ownerOf(_tokenId1) != ownerOf(_tokenId2), "You cannot fight your own Character.");
-        require(_characterDetails[_tokenId1].life > 0 && _characterDetails[_tokenId1].life > 0, "You can only fight living character.");
-        
-        //Make calculs 
+        require(ownerOf(_tokenId1) == msg.sender, "Not your character");
+        require(ownerOf(_tokenId1) != ownerOf(_tokenId2), "You cannot fight your own character.");
+        require(_characterDetails[_tokenId1].life > 0 && _characterDetails[_tokenId2].life > 0, "You can only fight living character.");
+
+        //Calculs 
         uint substractLifeToCharacter2 = (_characterDetails[_tokenId1].attack * _characterDetails[_tokenId1].experience) - (_characterDetails[_tokenId2].defense / 4);
         uint substractLifeToCharacter1 = (_characterDetails[_tokenId2].attack * _characterDetails[_tokenId2].experience) - (_characterDetails[_tokenId1].defense / 4);
 
-        //
+        //timestamp 
         _characterDetails[_tokenId1].lastFight = block.timestamp;
         _characterDetails[_tokenId2].lastFight = block.timestamp;
 
-        //Possibilité 1 : Le perso 1 tue le perso 2, le perso 2 ne peut répliquer
+        //Le perso 1 tue le perso 2, le perso 2 ne peut pas répliquer 
         if(_characterDetails[_tokenId2].life - substractLifeToCharacter2 <= 0) {
             _characterDetails[_tokenId2].life = 0;
-            _characterDetails[_tokenId1].experience += 1;
+            _characterDetails[_tokenId1].experience++;
         }
         else {
-            //Possibilité 2 : Le perso 1 ne tue pas le perso 2 mais le perso 2 réplique et le tue
-            if(_characterDetails[_tokenId2].life - substractLifeToCharacter2 > 0 
-            && _characterDetails[_tokenId1].life - substractLifeToCharacter1 <= 0) {
-                //On enlève les points de vie du perso 2
+            //Le perso 1 ne tue pas le perso 2, mais le perso 2 réplique et le tue 
+            if(_characterDetails[_tokenId2].life - substractLifeToCharacter2 > 0 && _characterDetails[_tokenId1].life - substractLifeToCharacter1 <= 0) {
                 _characterDetails[_tokenId2].life -= substractLifeToCharacter2;
-                //On met les points de vie du perso 1 à 0
                 _characterDetails[_tokenId1].life = 0;
-                _characterDetails[_tokenId2].experience += 1;
+                _characterDetails[_tokenId2].experience++;
             }
-            //Possibilité 3 : Le perso 1 ne tue pas le perso 2 et le perso 2 qui réplique ne le tue pas non plus
+            //Le perso 1 ne tue pas le perso 2 et que le perso 2 réplique sans tuer le perso 1
             else {
                 _characterDetails[_tokenId2].life -= substractLifeToCharacter2;
                 _characterDetails[_tokenId1].life -= substractLifeToCharacter1;
                 if(substractLifeToCharacter1 > substractLifeToCharacter2) {
-                    _characterDetails[_tokenId2].experience += 1;
+                    _characterDetails[_tokenId2].experience++;
                 }
                 else if(substractLifeToCharacter2 > substractLifeToCharacter1) {
-                    _characterDetails[_tokenId1].experience += 1;
+                    _characterDetails[_tokenId1].experience++;
                 }
                 else {
-                    _characterDetails[_tokenId2].experience += 1;
-                    _characterDetails[_tokenId1].experience += 1;
+                    _characterDetails[_tokenId1].experience++;
+                    _characterDetails[_tokenId2].experience++;
+
                 }
             }
         }
